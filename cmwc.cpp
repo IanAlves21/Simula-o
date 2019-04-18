369ead1f92282cf8360e6955d191e4d3880eccff
IanALves21 <ian.alves21@gmail.com>
2019-04-18 07:32:44 -0400

    first commit

--- cmwc.cpp
+++ cmwc.cpp
@@ -1,29 +1,29 @@
 // C99 Complementary Multiply With Carry generator
-// C99 Complementar Multiplicar com o gerador Carry 
+// C99 Complementar Multiplicar com o gerador Carry
 #include <bits/stdc++.h>
 
 // CMWC working parts
-// Peças de trabalho CMWC 
-#define CMWC_CYCLE 4096 // as Marsaglia recommends // como Marsaglia recomenda 
-#define CMWC_C_MAX 809430660 // as Marsaglia recommends // como a Marsaglia recomenda 
+// Peças de trabalho CMWC
+#define CMWC_CYCLE 4096 // as Marsaglia recommends // como Marsaglia recomenda
+#define CMWC_C_MAX 809430660 // as Marsaglia recommends // como a Marsaglia recomenda
 
 struct cmwc_state{
 	uint32_t Q[CMWC_CYCLE];
-	uint32_t c;	// must be limited with CMWC_C_MAX // deve ser limitado com CMWC_C_MAX 
+	uint32_t c;	// must be limited with CMWC_C_MAX // deve ser limitado com CMWC_C_MAX
 	unsigned i;
 };
 
 // Make 32 bit random number (some systems use 16 bit RAND_MAX [Visual C 2012 uses 15 bits!])
-//Cria  um número aleatório de 32 bits (alguns sistemas usam RAND_MAX de 16 bits [Visual C 2012 usa 15 bits!]) Uint32_t rand32 ( void ) 
+//Cria  um número aleatório de 32 bits (alguns sistemas usam RAND_MAX de 16 bits [Visual C 2012 usa 15 bits!]) Uint32_t rand32 ( void )
 uint32_t rand32(void){
 	uint32_t result = rand();
 	return result << 16 | rand();
 }
 
 // Init the state with seed
-// Inicia o estado com semente 
+// Inicia o estado com semente
 void initCMWC(struct cmwc_state *state, unsigned int seed){
-	srand(seed);        
+	srand(seed);
 	for (int i = 0; i < CMWC_CYCLE; i++)
 		state->Q[i] = rand32();
 	do
@@ -33,10 +33,10 @@ void initCMWC(struct cmwc_state *state, unsigned int seed){
 }
 
 // CMWC engine
-// Mecanismo CMWC 
-uint32_t randCMWC(struct cmwc_state *state){  //EDITED parameter *state was missing // O parâmetro EDITED * state estava ausente 
-	uint64_t const a = 18782; // as Marsaglia recommends  // como Marsaglia recomenda 
-	uint32_t const m = 0xfffffffe; // as Marsaglia recommends // como Marsaglia recomenda 
+// Mecanismo CMWC
+uint32_t randCMWC(struct cmwc_state *state){  //EDITED parameter *state was missing // O parâmetro EDITED * state estava ausente
+	uint64_t const a = 18782; // as Marsaglia recommends  // como Marsaglia recomenda
+	uint32_t const m = 0xfffffffe; // as Marsaglia recommends // como Marsaglia recomenda
 	uint64_t t;
 	uint32_t x;
 
@@ -58,8 +58,10 @@ int main(){
 	unsigned int seed = time(NULL);
 
 	initCMWC(&cmwc, seed);
-    
-    for(int h=0; h<10; h++){
-	    printf("Random CMWC: %u\n", (randCMWC(&cmwc)%10));
+	uint32_t num;
+    for(int h=0; h<1000; h++){
+		num = randCMWC(&cmwc);
+		// printf("Random CMWC: %u\n", (randCMWC(&cmwc)%10));
+	    printf("%u %u\n",(num),(num%10));
     }
 }
