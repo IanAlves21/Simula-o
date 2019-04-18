50492b12d127bcc0f5d6c548cb1aaad01c57435e
Ian Alves <igaps.eng17@uea.edu.br>
2019-04-18 19:45:33 -0400

    first commit

--- chi-quadrado.cpp
+++ chi-quadrado.cpp
@@ -18,20 +18,21 @@ int main(){
         // cout<<ignore<<elemento<<endl;
         freq_Observada[elemento]++;
     }
-    cout<<"frequencia do 0: "<<freq_Observada[0]<<endl;
-    cout<<"frequencia do 1: "<<freq_Observada[1]<<endl;
-    cout<<"frequencia do 2: "<<freq_Observada[2]<<endl;
-    cout<<"frequencia do 3: "<<freq_Observada[3]<<endl;
-    cout<<"frequencia do 4: "<<freq_Observada[4]<<endl;
-    cout<<"frequencia do 5: "<<freq_Observada[5]<<endl;
-    cout<<"frequencia do 6: "<<freq_Observada[6]<<endl;
-    cout<<"frequencia do 7: "<<freq_Observada[7]<<endl;
-    cout<<"frequencia do 8: "<<freq_Observada[8]<<endl;
-    cout<<"frequencia do 9: "<<freq_Observada[9]<<endl;
-    cout<<freq_Esperada<<endl;
+    cout<<"Distribuição de "<<(freq_Esperada*10)<<"elementos"<<endl<<endl;
+    cout<<"Frequencia do 0: "<<freq_Observada[0]<<endl;
+    cout<<"Frequencia do 1: "<<freq_Observada[1]<<endl;
+    cout<<"Frequencia do 2: "<<freq_Observada[2]<<endl;
+    cout<<"Frequencia do 3: "<<freq_Observada[3]<<endl;
+    cout<<"Frequencia do 4: "<<freq_Observada[4]<<endl;
+    cout<<"Frequencia do 5: "<<freq_Observada[5]<<endl;
+    cout<<"Frequencia do 6: "<<freq_Observada[6]<<endl;
+    cout<<"Frequencia do 7: "<<freq_Observada[7]<<endl;
+    cout<<"Frequencia do 8: "<<freq_Observada[8]<<endl;
+    cout<<"Frequencia do 9: "<<freq_Observada[9]<<endl<<endl;
+    cout<<"Frequencia esperada: "<<freq_Esperada<<endl;
     for(int i=0; i<10; i++){
         chi_quadrado += ((pow(freq_Observada[i]-freq_Esperada, 2))/(freq_Esperada));
     }
     cout<<endl;
-    cout<<"CHI QUADRADO: "<<chi_quadrado<<endl;
+    cout<<"teste do CHI-QUADRADO: "<<chi_quadrado<<endl;
 }
